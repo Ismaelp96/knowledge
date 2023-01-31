2ab4ef72b99d3451385f10e84683ae83029615a6
Ismael Oliveira <ismael6991@live.com>
2023-01-31 15:34:34 -0300

    Adicionando uma consulta por ID de cada categoria na árvore de artigos

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
 
   // cuidado com a ordem! tem que ser antes de categories/:id
   app.route('/categories/tree').get(app.api.category.getTree)
+
   app
     .route('/categories/:id')
     .get(app.api.category.getById)
@@ -14,9 +15,12 @@ module.exports = (app) => {
     .delete(app.api.category.remove)
 
   app.route('/articles').get(app.api.article.get).post(app.api.article.save)
+
   app
     .route('/articles/:id')
     .get(app.api.article.getById)
     .put(app.api.article.save)
     .delete(app.api.article.remove)
+
+  app.route('/categories/:id/articles').get(app.api.article.getByCategory)
 }
