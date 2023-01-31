2e930420d58260e8c84c7c64a27700c610a8f840
Ismael Oliveira <ismael6991@live.com>
2023-01-30 21:11:39 -0300

    Api de categoria #02, criando árvore de caminho ref. as categorias

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
 
   app.route('/categories').get(app.api.category.get).post(app.api.category.save)
 
+  // cuidado com a ordem! tem que ser antes de categories/:id
+  app.route('/categories/tree').get(app.api.category.getTree)
   app
     .route('/categories/:id')
     .get(app.api.category.getById)
