bb1bf3ffa5632f5cf01689a55c132e3be66ef24f
Ismael Oliveira <ismael6991@live.com>
2023-02-01 12:31:36 -0300

    resolvendo bug de coneção com o mongoDB

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', true)
-
 mongoose
-  .connect('mongodb://localhost:27017/knowledge_stats', {
+  .connect('mongodb://127.0.0.1:27017/knowledge_stats', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .catch((e) => {
     const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
